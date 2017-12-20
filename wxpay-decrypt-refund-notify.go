7da729136f5019d3c84e0061841d2016bb621931
bobymxiang(向一鸣) <bobymxiang@tencent.com>
2017-12-20 16:54:00 +0800

    fixed: Pkcs7Padding instead of NoPadding. added two depends

--- wxpay-decrypt-refund-notify.go
+++ wxpay-decrypt-refund-notify.go
@@ -2,75 +2,17 @@ package main
 
 import (
 	"crypto/aes"
-	"crypto/cipher"
 	"crypto/md5"
 	"encoding/base64"
 	"encoding/hex"
 	"fmt"
+
+	"github.com/andreburgaud/crypt2go/ecb"
+	"github.com/andreburgaud/crypt2go/padding"
 )
 
-type ecb struct {
-	b         cipher.Block
-	blockSize int
-}
-
-func newECB(b cipher.Block) *ecb {
-	return &ecb{
-		b:         b,
-		blockSize: b.BlockSize(),
-	}
-}
-
-type ecbEncrypter ecb
-
-// NewECBEncrypter returns a BlockMode which encrypts in electronic code book
-// mode, using the given Block.
-func NewECBEncrypter(b cipher.Block) cipher.BlockMode {
-	return (*ecbEncrypter)(newECB(b))
-}
-
-func (x *ecbEncrypter) BlockSize() int { return x.blockSize }
-
-func (x *ecbEncrypter) CryptBlocks(dst, src []byte) {
-	if len(src)%x.blockSize != 0 {
-		panic("crypto/cipher: input not full blocks")
-	}
-	if len(dst) < len(src) {
-		panic("crypto/cipher: output smaller than input")
-	}
-	for len(src) > 0 {
-		x.b.Encrypt(dst, src[:x.blockSize])
-		src = src[x.blockSize:]
-		dst = dst[x.blockSize:]
-	}
-}
-
-type ecbDecrypter ecb
-
-// NewECBDecrypter returns a BlockMode which decrypts in electronic code book
-// mode, using the given Block.
-func NewECBDecrypter(b cipher.Block) cipher.BlockMode {
-	return (*ecbDecrypter)(newECB(b))
-}
-
-func (x *ecbDecrypter) BlockSize() int { return x.blockSize }
-
-func (x *ecbDecrypter) CryptBlocks(dst, src []byte) {
-	if len(src)%x.blockSize != 0 {
-		panic("crypto/cipher: input not full blocks")
-	}
-	if len(dst) < len(src) {
-		panic("crypto/cipher: output smaller than input")
-	}
-	for len(src) > 0 {
-		x.b.Decrypt(dst, src[:x.blockSize])
-		src = src[x.blockSize:]
-		dst = dst[x.blockSize:]
-	}
-}
-
-var key = ""
-var crypted = ""
+var key = "nfy899iQXpBuEQATHwWVmpXTqZf6vfCL"
+var crypted = "wLjKN3LwcHFGLycZOCO5sE0PYURUZ/LXBuR0IJFe7KQgx9+Ndl7RIR9ag1H+vDNa8ymegl23iLKsyF13hAM9qXr1BfOTpEYA7Xp0cC/WA39hgUVB5tUuksXAaVXUKx7oU0cq3mmFD39M3MtorXu0510/PUXwuOM8Jg5QLpa8NjeMAgyxX+MRMtj+CnEun4ei6FpdfTiZLPptkJrEEsP4TOAVpruYnYR4Eu+p/MTvkZJh5VEhSpfq/xiVZee8Zl0Ez+jqgQt+q7fpnpwvSzUgahoLUZqvauBxFnmcyLorqyCvvD6ZbWF5khI/HOnmiBx2ijfEyL0vIt0YTOW21qWR85Jq4krD/q1LGlv9QJddwH43Xl017qnoq8oYoO+rkIPM6GvEZfS+kMWztzOtmh/ZOA30+wO0c06413Aw2K14IpcEmLXtYGn/hxw7gPxGe22efq6/PbT88I0pYrPSnkjaeRI/4hAk6iHvjBs8kjHXcCM84x08W5ZJ7PePqkS1Ggn5ZWekvVQyeI/3khTXauJU+mwCF0K6th0cDVKzWcRQJgrRiHLFC3TPV/AUxPIwiIXETkN6KRKQuWPkL07quMy5cAB1Y8h9RtXsVub6nTyqSfdoUBLHsOE1BNq863e8wEbS57nUaNS/3WppaZz7qpRoxs/o6oELfqu36Z6cL0s1IGomnvhLrxzaQ0LQrbQtReLP4nhiuiFqJoKJOWCFm2uuME2l1Qson3vmlnQg6ObvGTrRLd690qyfnm+GvIDEQi+wS6jpF4vr5lwsT+PXYa1cnNaZML0akxSGFGksTXF30QvIONv/fgW7D7gPtHLMG1xQxcovacpQ4Q86oHqnt6HP8aIB4OyfdKZZEvhXGBVspG7m6iyR6CiuVvpiQlT6E3Zq0S3evdKsn55vhryAxEIvsFDAGGbyZ1aSzYAbx1ao51jw8fopL/t+2+ifWFQjOrUJWVqkRWpUTbG+DX8PfpKgVEaRbviOifo5D5ggbuek5azUCThV1UP0VdsvhMrj+yrPgnZZo2Ae96137587kQ6yBalYuAnVEzQvZRfFdtw0ong="
 
 func init() {
 
@@ -89,9 +31,14 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	mode := NewECBDecrypter(block)
+	mode := ecb.NewECBDecrypter(block)
 	content := []byte(deBase64)
 	mode.CryptBlocks(content, content)
+	padder := padding.NewPkcs7Padding(mode.BlockSize())
+	content, err = padder.Unpad(content)
+	if err != nil {
+		panic(err.Error())
+	}
 
 	fmt.Printf("%s\n", content)
 }
